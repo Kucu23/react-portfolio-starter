4670778ff577b93eca2309257b40df09fb3c8ce2
Kucu23 <mi.kuczynski@gmail.com>
2023-07-24 19:21:38 +0200

    changed ports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
 export default defineConfig({
-  plugins: [react()],
-})
+server: {
+host: true,
+port: 80 ,
+},
+});
